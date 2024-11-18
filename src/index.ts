e14c249bde77c19f1da1d71d816a13432f5097d7
bonanoo <ainaraspirmas@gmail.com>
2024-11-18 22:29:49 +0200

    oopsies

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,3 @@ import "reflect-metadata";
 import { Client } from "./client";
 export * from "./client";
 export * from "./proto";
-
-const client = new Client({
-	address: "127.0.0.1",
-	port: 19132,
-	mtuSize: 1492,
-});
-
-console.time("connect");
-client.connect().then((ad) => {
-	console.timeEnd("connect");
-	console.log(ad);
-});
