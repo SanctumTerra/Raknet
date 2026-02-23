9ed35524853af77b753a52143c4a64bcc13196c9
AnyBananaGAME <ainaraspirmas@gmail.com>
2026-02-23 11:52:37 +0200

    .

--- src/tests/client-once.ts
+++ src/tests/client-once.ts
@@ -4,12 +4,7 @@ const client = new Client({
 	address: "donutsmp.net",
 	// address: "127.0.0.1",
 	// address: "geo.hivebedrock.network",
-	proxy: {
-		host: "216.185.39.231",
-		port: 45849,
-		userId: "CV8GK4TW",
-		password: "I2F91P8H",
-	},
+	
 });
 
 client.on("unconnectedPong", (packet) => {
